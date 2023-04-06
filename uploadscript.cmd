6d165806ded2a3602e30bf362a221406428e86d1
Predator <predator_x_x_x@mail.ru>
2023-04-06 20:33:54 +1100

    upload scripts

--- uploadscript.cmd
+++ uploadscript.cmd
@@ -1,11 +1,8 @@
-open sftp://%1%:%2%@79.133.182.230/ -hostkey="ssh-rsa 3072 5qrDQiJO1pKKih3vR7TC/gDywG87UqYolKyhWcMFc0M"
-cd /home/%1%/shop_be/public
-call mkdir ../admin 2>/dev/null || :
+open sftp://%1%:%2%@151.248.122.7/ -hostkey="ssh-rsa 3072 wTIRAYg366AK+7qm1su/eueZYX2Gc5f3r4VG1g0Du28"
+cd /home/%1%/ooo-egeon-be/public
 call mkdir ../jsons 2>/dev/null || :
-call cp -rf admin ../admin 2>/dev/null || :
 call cp -f *.json ../jsons/ 2>/dev/null || :
 call rm -rf * 2>/dev/null || :
-call mv ../admin admin 2>/dev/null || :
 call mv ../jsons/*.json ./ 2>/dev/null || :
 put public.7z
 call 7z x public.7z -y
