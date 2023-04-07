98d35030877fc58f513879df1f5a0a9dffd6eee0
Predator <predator_x_x_x@mail.ru>
2023-04-07 17:58:02 +1100

    works img

--- src/components/pages/works/Works.vue
+++ src/components/pages/works/Works.vue
@@ -4,9 +4,13 @@
             <v-card class="cardColor ma-1 ma-sm-8 px-2 px-sm-12">
                 <div class="text-h6 text-lg-h3 text-center mt-8 font-weight-black">{{ work.title }}</div>
                 <div class="text-body-1 text-lg-h4 ma-lg-14 font-weight-black">
-                        <div v-html="work.text"></div>
+                    <div v-html="work.text"></div>
                 </div>
-                <v-img v-for="(img, idx) in work.pictures" :key="idx" class="my-2 my-sm-8" :src="`/pictures/works/${img}`"></v-img>
+                <!-- <v-img v-for="(img, idx) in work.pictures" :key="idx" :min-width="$isMobile ? '100%' : '75%'" class="my-2 my-sm-8" :src="`/pictures/works/${img}`"></v-img> -->
+                <div v-for="(img, idx) in work.pictures" :key="idx"
+                    class="d-flex justify-center my-2 my-sm-12">
+                    <img :src="`/pictures/works/${img}`" :style="$isMobile ? 'min-width:100%' : 'min-width:75%'" />
+                </div>
             </v-card>
         </div>
     </v-parallax>
